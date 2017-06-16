84c6b3d5ceab982fd4ff7d16682a6648891a78c1
Aidan Dempster <aidan.dempster@gmail.com>
2017-06-16 09:37:35 -0400

    Added humorous route

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import { Route, Switch } from 'react-router';
 import Home from './components/Home';
 import About from './components/About';
 import NotFound from './components/NotFound';
+import NoExample from './components/NoExample';
 
-const Routes = (props) => (
+const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/NoExample" component={NoExample}/>
     <Route path="*" component={NotFound} />
   </Switch>
 );
